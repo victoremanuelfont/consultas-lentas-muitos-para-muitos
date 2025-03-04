41a240849ece17309b6f1d7b200044cca605b082
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-03-04 11:09:40 -0300

    JOIN FETCH resolvendo o problema n+1 consultas

--- src/main/java/com/example/demo/repositories/CategoryRepository.java
+++ src/main/java/com/example/demo/repositories/CategoryRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.repositories;
 
+import com.example.demo.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+
+
 
 }
